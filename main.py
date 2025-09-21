7da44ce7ba1d835f9dadc0a0f9dd667f6f3d2f7b
kkdieker-fhsu <kkdieker@mail.fhsu.edu>
2025-09-21 07:29:47 -0800

    comments and first chart

--- main.py
+++ main.py
@@ -19,13 +19,15 @@
 #     (10/10 points) I will be checking out the main branch of your project. Please be sure to include a requirements.txt file which contains all the packages that need installed. You can create this fille with the output of pip freeze at the terminal prompt.
 #     (20/20 points) There should be a README.md file in your project that explains what your project is, how to install the pip requirements, and how to execute the program. Please use the GitHub flavor of Markdown. Be thorough on the explanations.
 
+# This project is to visualize and quantify how lucky or unlucky I am when playing TTRPGs. The data is from the virtual
+#   tabletop I host and includes my roll data from the last several sessions.
 
 import matplotlib.pyplot as plt
 import pandas as pd
 import os
 
 # this is probably an awful way of parsing this, but the json data used contains quite a few different records, with the
-#   one I wanted buried inside other parts of it.
+#   one I wanted buried inside other parts of it
 
 # first, read the data in the json file that is formatted as 'records'
 data = pd.read_json('me-dice-stats-json-data.json', orient='records')
@@ -41,23 +43,34 @@ for i in range(len(datadict['ROLLS'])):
     tuples.append((i+1,datadict['ROLLS'][i]))
 total_rolls = datadict['TOTAL_ROLLS']
 
-# finally, recreate the dataframe with the values desired
+# finally, recreate the dataframe with the values desired and get some useful values from it
 rolldata = pd.DataFrame.from_records(tuples, columns=['Die Value', 'Frequency'])
 diesize = len(rolldata)
 expectedvalue = float(total_rolls)/float(diesize)
 
+# begin plotting
 fig, ax = plt.subplots()
 ax.set_xlabel('Roll')
 ax.set_xticks(ticks=rolldata['Die Value'])
 ax.set_ylabel('Frequency')
 ax.set_yticks(ticks=range(1,max(rolldata['Frequency'])+1))
 ax.set_title('Frequency of Dice rolls')
-ax.bar(rolldata['Die Value'], rolldata['Frequency'])
-ax.axhline(y=expectedvalue, color='r')
 
+# create a bar chart with a horizontal line across it denoting the expected value of the die
+ax.bar(rolldata['Die Value'], rolldata['Frequency'], color='black')
+ax.axhline(y=expectedvalue, color='g')
+
+# this part is to create the bars showing the distance of each result from the expected value using error bars
+# first, get the difference for each data point
 ydiffs = [expectedvalue] * diesize - rolldata['Frequency']
+
+# create lists for the upper and lower error to get asymmetric error bars
 ylowererror = []
 yuppererror = []
+
+# loop over each difference
+# if the difference is negative, the frequency is higher than expected, so that difference
+#   is made positive and appended to the lower error list and a 0 to the upper, and vice versa for positive differences
 for e in ydiffs:
     if e < 0:
         ylowererror.append(abs(e))
@@ -66,7 +79,9 @@ for e in ydiffs:
         yuppererror.append(e)
         ylowererror.append(0)
 
+# create the error array for plotting
 yerror = [ylowererror, yuppererror]
 
-ax.errorbar(rolldata['Die Value'], rolldata['Frequency'], yerr = yerror, fmt='none', color='r', capsize=7)
+# plot the error bars
+ax.errorbar(rolldata['Die Value'], rolldata['Frequency'], yerr = yerror, fmt='none', color='r')
 fig.show()
